5d04988a9cd9c9e5f21cbf19f99b5f99b002b8bd
riyenas0925 <riyenas0925@gmail.com>
2022-03-12 13:41:33 +0900

    test: data process 테스트를 Parameterized 테스트로 변경

--- src/test/java/dev/riyenas/assignment/service/DataProcessServiceTest.java
+++ src/test/java/dev/riyenas/assignment/service/DataProcessServiceTest.java
@@ -2,7 +2,8 @@ package dev.riyenas.assignment.service;
 
 import dev.riyenas.assignment.domain.Result;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 
@@ -14,18 +15,23 @@ public class DataProcessServiceTest{
     @Autowired
     private DataProcessService dataProcessService;
 
-    @Test
+    @ParameterizedTest
+    @CsvSource(value = {
+            "!@#$%^&*()_+ fedcba FEDCBA 654321:5:A1a2B3b4C5c6DdE:eFf",
+            "A0a1B2b3C4c5:5:A0a1B2b3C4:c5",
+            "A0a1B2b3C4c5:12:A0a1B2b3C4c5:''",
+            "A0a1B2b3C4c5:13:'':A0a1B2b3C4c5"
+    }, delimiter = ':')
     @DisplayName("크롤링된 데이터를 영어와 숫자를 가공해 몫과 나머지를 구한다.")
-    public void dataProcess() {
+    public void dataProcess(String data, int unit, String quotient, String reminder) {
         // given
-        String data = "!@#$%^&*()_+ fedcba FEDCBA 654321";
 
         // when
-        Result expected = dataProcessService.dataProcess(data, 5);
+        Result expected = dataProcessService.dataProcess(data, unit);
 
         // that
-        assertThat("A1a2B3b4C5c6DdE").isEqualTo(expected.getQuotient());
-        assertThat("eFf").isEqualTo(expected.getReminder());
+        assertThat(quotient).isEqualTo(expected.getQuotient());
+        assertThat(reminder).isEqualTo(expected.getReminder());
     }
 
 }
